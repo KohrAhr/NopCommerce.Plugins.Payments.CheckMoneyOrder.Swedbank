997870490c4b6e5b6ed82e05b6e7bc5cc300cdb1
MonsterZam <github@1click.lv>
2023-11-23 17:28:13 +0200

    Initial commit, based on standard CheckMoneyOrder component

---
+++ Components/CheckMoneyOrderPaymentInfoViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Plugin.Payments.CheckMoneyOrder.Swedbank.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Payments.CheckMoneyOrder.Swedbank.Components
+{
+    [ViewComponent(Name = "CheckMoneyOrder.Swedbank")]
+    public class CheckMoneyOrderViewComponent : NopViewComponent
+    {
+        private readonly CheckMoneyOrderSwedbankPaymentSettings _checkMoneyOrderPaymentSettings;
+        private readonly ILocalizationService _localizationService;
+        private readonly IStoreContext _storeContext;
+        private readonly IWorkContext _workContext;
+
+        public CheckMoneyOrderViewComponent(CheckMoneyOrderSwedbankPaymentSettings checkMoneyOrderPaymentSettings,
+            ILocalizationService localizationService,
+            IStoreContext storeContext,
+            IWorkContext workContext)
+        {
+            _checkMoneyOrderPaymentSettings = checkMoneyOrderPaymentSettings;
+            _localizationService = localizationService;
+            _storeContext = storeContext;
+            _workContext = workContext;
+        }
+
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var store = await _storeContext.GetCurrentStoreAsync();
+
+            var model = new PaymentInfoModel
+            {
+                DescriptionText = await _localizationService.GetLocalizedSettingAsync(_checkMoneyOrderPaymentSettings,
+                    x => x.DescriptionText, (await _workContext.GetWorkingLanguageAsync()).Id, store.Id)
+            };
+
+            return View("~/Plugins/Payments.CheckMoneyOrder.Swedbank/Views/PaymentInfo.cshtml", model);
+        }
+    }
+}
